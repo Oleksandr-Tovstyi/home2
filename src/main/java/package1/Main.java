5a00fe713f128811d91543900e3ae74aaf6b30d1
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-06-05 16:29:48 +0300

    pom.xml was changed <properties java 17, UTF-8>

---
+++ src/main/java/package1/Main.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class Main{
+    public static void main(String[] args) {
+        NewYear ny = new NewYear();
+        System.out.println(ny.nextHappyYear(1001));
+    }
+}
---
+++ src/main/java/package1/NewYear.java
@@ -0,0 +1,30 @@
+package package1;
+
+public class NewYear {
+
+    public int ne;
+    public int nextHappyYear(int year) {
+        // Your code is here ... See you Next Happy Year !!!
+        System.out.println("year = " + year);
+        boolean flag = true;
+        while (flag){
+            year++;
+            boolean isNewYear = false;
+            char[] ch = String.valueOf(year).toCharArray();
+            for(int i = 0; i < ch.length - 1; i++){
+                for(int j = i + 1; j < ch.length; j++){
+                    if(ch[i] == ch[j]) {
+                        isNewYear = true;
+                    }
+                }
+            }
+            if(isNewYear){
+                isNewYear = false;
+                continue;
+            } else {
+                return year;
+            }
+        }
+        return 1;
+    }
+}
