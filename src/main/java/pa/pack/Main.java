192ba6cab770b9b93f244075c28382e00d6dfcb2
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-06-06 14:03:31 +0300

    added checkstyle to pom.xml

--- src/main/java/pa/pack/Main.java
+++ src/main/java/pa/pack/Main.java
@@ -1,6 +1,6 @@
-package package1;
+package pa.pack;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         NewYear ny = new NewYear();
         System.out.println(ny.nextHappyYear(1001));
--- src/main/java/pa/pack/NewYear.java
+++ src/main/java/pa/pack/NewYear.java
@@ -1,24 +1,25 @@
-package package1;
+package pa.pack;
 
 public class NewYear {
 
-    public int ne;
+    private int ne;
+
     public int nextHappyYear(int year) {
         // Your code is here ... See you Next Happy Year !!!
         System.out.println("year = " + year);
         boolean flag = true;
-        while (flag){
+        while (flag) {
             year++;
             boolean isNewYear = false;
             char[] ch = String.valueOf(year).toCharArray();
-            for(int i = 0; i < ch.length - 1; i++){
-                for(int j = i + 1; j < ch.length; j++){
-                    if(ch[i] == ch[j]) {
+            for (int i = 0; i < ch.length - 1; i++) {
+                for (int j = i + 1; j < ch.length; j++) {
+                    if (ch[i] == ch[j]) {
                         isNewYear = true;
                     }
                 }
             }
-            if(isNewYear){
+            if (isNewYear) {
                 isNewYear = false;
                 continue;
             } else {
